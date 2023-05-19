576c1f166003fabfaa6c183b3f7af3e8f858f5be
Ava Abderezaei <ava.abderezaei@gmail.com>
2023-05-19 15:58:20 -0600

    Add fake data and their plots

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to run
+```
+conda create --name <env> --file requirements.txt
+python main.py
+```
--- data.py
+++ data.py
@@ -1,16 +1,79 @@
 import tensorflow as tf
 import numpy as np
 
-'''
-T max = 10 second
-dt = 0.02 second
-max trajectory lenght = 10/0.02 = 500
-number of features per step = 15 (x, xdot, xddot, o, odot) x=(x, y, z), o=(phi, thetha, psi)
-'''
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-MAX_TRAJ_STEPS = 500
-EMBED_DIM = 15
-DATA_NUM = 3
+T_max = 3
+dt = 0.02
+
+MAX_TRAJ_STEPS = int(T_max//dt) # 150
+EMBED_DIM = 16 # (x, xdot, xddot, o, odot, cup_type) x=(x, y, z), o=(phi, thetha, psi) cup_type = 1 or 2 or 3
+DATA_NUM = 2
+
+
+def get_initials(data_num):
+    if data_num == 0:
+        x, y, z = 0.2, -1, 0.1
+        xdot, ydot, zdot = 0, 0, 0
+        xddot, yddot, zddot = 0, 0.4, 0.3
+        y_train = 1
+    else:
+        x, y, z = 0.2, -1, 0.1
+        xdot, ydot, zdot = 0, 0, 0
+        xddot, yddot, zddot = 1, 3, 10
+        y_train = 0
+    return y_train, x, y, z, xdot, ydot, zdot, xddot, yddot, zddot
+
+
+def generate_fake_data():
+    # Rough estimate of Panda's reachable area: x: (0, 1.57), y: (-1, 1), z: (0, 1)
+    x_train = np.zeros((DATA_NUM, MAX_TRAJ_STEPS, EMBED_DIM), dtype=np.float64)
+    y_train = np.zeros((DATA_NUM, 1, 1))
+    
+    phi, theta, psi = 0, 0, 0
+    phidot, thetadot, psidot = 0, 0, 0
+    cup_type = 1
+
+    for d in range(DATA_NUM):
+        y_train[d], x, y, z, xdot, ydot, zdot, xddot, yddot, zddot = get_initials(d)
+        zddot_slow_down = 3.7*zddot/(MAX_TRAJ_STEPS//2)
+        
+        for i in range(MAX_TRAJ_STEPS):
+            x_train[d, i, :] = np.array([x, xdot, xddot, y, ydot, yddot, z, zdot, zddot, phi, phidot, theta, thetadot, psi, psidot, cup_type])
+
+            x += xdot * dt
+            y += ydot * dt
+            z += zdot * dt
+            xdot += xddot * dt
+            ydot += yddot * dt
+            zdot += zddot * dt
+            
+            if i >= MAX_TRAJ_STEPS//4 and i <= 3 * MAX_TRAJ_STEPS//4:
+                zddot -= zddot_slow_down
+
+
+    return x_train, y_train
+
+
+def visualize_data(x_train, y_train):
+    # for d in range(DATA_NUM):
+    d = 0
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim([0, 1.57])
+    ax.set_ylim([-1, 1])
+    ax.set_zlim([0, 1])
+    ax.scatter(x_train[d, :, 0], x_train[d, :, 3], x_train[d, :, 6])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    # plt.show()
+    plt.savefig('data_safe_trajectory.png')
+
+# x_train, y_train = generate_fake_data()
+# visualize_data(x_train, y_train)
+
 
 def get_data(debug=False):
     if debug:
