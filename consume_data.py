ed9fd817df0f7b75f0b31996c194921273d95302
Ava Abderezaei <ava.abderezaei@gmail.com>
2023-05-26 22:22:37 -0600

    Automate experiments

--- consume_data.py
+++ consume_data.py
@@ -15,113 +15,56 @@ import argparse
 import time
 
 import attr
-
 import natnet
-
 import csv
+import atexit
+import numpy as np
 
-from constants import DATA_DIR
+from constants import DATA_DIR, E_ID
+
+csvfile = open('data/data_may_26.csv', '+a', newline='\n')
+writer = csv.writer(csvfile)
+
+def exit_handler():
+    print('Closing file...')
+    csvfile.close()
+
+atexit.register(exit_handler)
 
 @attr.s
 class ClientApp(object):
 
     _client = attr.ib()
     _quiet = attr.ib()
-
     _last_printed = attr.ib(0)
 
     @classmethod
-    def connect(cls, server_name, rate, quiet):
-        if server_name == 'fake':
-            client = natnet.fakes.SingleFrameFakeClient.fake_connect(rate=rate)
-        elif server_name == 'fake_v2':
-            client = natnet.fakes.SingleFrameFakeClient.fake_connect_v2(rate=rate)
-        else:
-            client = natnet.Client.connect(server_name)
+    def connect(cls, server_name=None, quiet=False):
+        client = natnet.Client.connect(server_name)
         if client is None:
             return None
         return cls(client, quiet)
 
     def run(self):
-        if self._quiet:
-            self._client.set_callback(self.callback_quiet)
-        else:
-            self._client.set_callback(self.callback)
+        self._client.set_callback(self.callback)
         self._client.spin()
 
     def callback(self, rigid_bodies, skeletons, markers, timing):
-        """
+        experiment_id = E_ID
 
-        :type rigid_bodies: list[RigidBody]
-        :type rigid_bodies: list[Skeleton]
-        :type markers: list[LabelledMarker]
-        :type timing: TimestampAndLatency
-        """
-        print()
-        print('{:.1f}s: Received mocap frame'.format(timing.timestamp))
         if rigid_bodies:
-            print('Rigid bodies:')
-            with open(DATA_DIR, '+a', newline='\n') as csvfile:
-                for b in rigid_bodies:
-                    print('    Id {}: ({: 5.2f}, {: 5.2f}, {: 5.2f}), ({: 5.2f}, {: 5.2f}, {: 5.2f}, {: 5.2f})'.format(
-                        b.id_, *(b.position + b.orientation)
-                    ))
-                    writer = csv.writer(csvfile)
-                    writer.writerow(['7', timing.timestamp, *(b.position + b.orientation)])
-
-        if skeletons:
-            print('Skeletons:')
-            for s in skeletons:
-                print('    Id {}: {} bones'.format(s.id_, len(s.rigid_bodies)))
-                for b in s.rigid_bodies:
-                    # TODO: Is this just a skeleton thing? Should the ID be split at parse time?
-                    body_id = b.id_ & 0xFFFF
-                    parent_id = b.id_ >> 16
-                    message = '        Bone {:2d}: '.format(body_id)
-                    if parent_id != s.id_:
-                        message += 'parent {}, '.format(parent_id)
-                    message += '({: 5.2f}, {: 5.2f}, {: 5.2f}), ({: 5.2f}, {: 5.2f}, {: 5.2f}, {: 5.2f})'.format(
-                        *(b.position + b.orientation)
-                    )
-                    print(message)
-        if markers:
-            print('Markers:')
-            for m in markers:
-                print('    Model {} marker {:2d}: size {:7.4f}mm, pos ({: 5.2f}, {: 5.2f}, {: 5.2f}), '.format(
-                    m.model_id, m.marker_id, 1000*m.size, *m.position
-                ))
-        if timing.latency is not None:
-            print('Latency: {:.1f}ms (system {:.1f}ms, transit {:.1f}ms, processing {:.2f}ms)'.format(
-                1000*timing.latency, 1000*timing.system_latency, 1000*timing.transit_latency,
-                1000*timing.processing_latency
-            ))
-
-    def callback_quiet(self, *_):
-        if time.time() - self._last_printed > 1:
-            print('.')
-            self._last_printed = time.time()
-
+            for r in rigid_bodies:
+                x, y, z = r.position
+                a, b, c, d = r.orientation
+                timestamp = timing.timestamp        
+                print("Recieved: ", experiment_id, timestamp, x, y, z, a, b, c, d)
+                writer.writerow([experiment_id, timestamp, x, y, z, a, b, c, d])
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--server', help='Will autodiscover if not supplied')
-    # parser.add_argument('--fake', action='store_true',
-    #                     help='Produce fake data at `rate` instead of connecting to actual server')
-    # parser.add_argument('--fake-v2', action='store_true',
-    #                     help='Produce fake data at `rate` instead of connecting to actual server (NatNet 2.10)')
-    # parser.add_argument('--rate', type=float, default=10,
-    #                     help='Rate at which to produce fake data (Hz)')
-    # parser.add_argument('--quiet', action='store_true')
-    # args = parser.parse_args()
-
     try:
-        # server_name = args.server
-        # if args.fake:
-        #     server_name = 'fake'
-        # if args.fake_v2:
-        #     server_name = 'fake_v2'
-        app = ClientApp.connect(None, 0, False)
+        app = ClientApp.connect()
         app.run()
+    
     except natnet.DiscoveryError as e:
         print('Error:', e)
 
--- visualize_data.py
+++ visualize_data.py
@@ -6,11 +6,26 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from scipy.spatial.transform import Rotation as R
 from itertools import islice
+import math
 
-from constants import DATA_DIR
+from constants import DATA_DIR, E_ID
 
 def rotate_vector(vector, rotation_matrix):
     return np.dot(rotation_matrix, vector)
+
+def quaternion_to_euler_angle(w, x, y, z):
+    ysqr = y * y
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + ysqr)
+    X = math.degrees(math.atan2(t0, t1))
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    Y = math.degrees(math.asin(t2))
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (ysqr + z * z)
+    Z = math.degrees(math.atan2(t3, t4))
+    return X, Y, Z
 
 def calculate_endpoint(start, a, b, c, d):
     rotation_matrix = R.from_quat([a, b, c, d]).as_matrix()
@@ -18,18 +33,25 @@ def calculate_endpoint(start, a, b, c, d):
     endpoint = rotate_vector(unit_vector, rotation_matrix)
     return start + endpoint
 
-def read_experiment(experiment_id, num_samples=100):
+def read_experiment(data_dir, experiment_id, num_samples=100):
     start_points = np.empty((0, 3), np.float64)
     end_points = np.empty((0, 3), np.float64)
-    with open(DATA_DIR, mode ='r')as file:
+    with open(data_dir, mode ='r')as file:
         reader = csv.DictReader(file)
         for row in reader:
             keys = list(row.values())
             e_id, timestamp = int(keys[0]), keys[1]
             x, y, z = np.float64(keys[2]), np.float64(keys[3]), np.float64(keys[4]),
             a, b, c, d = np.float64(keys[5]), np.float64(keys[6]), np.float64(keys[7]), np.float64(keys[8])
+            
+            
 
+            # rot = R.from_quat([a, b, c, d])
+
+            
             if e_id == experiment_id:
+                print('euler angle: ', quaternion_to_euler_angle(a, b, c, d))
+
                 start_point = np.array([[x, y, z]])
                 end_point = calculate_endpoint(start_point, a, b, c, d)
                 start_points = np.append(start_points, start_point, axis=0)
@@ -43,6 +65,8 @@ def read_experiment(experiment_id, num_samples=100):
 def visualize_data_with_orientation(experiment_id, start_points, end_points, arrows_lenght=0.001):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    print(start_points[:, 0], start_points[:, 1], start_points[:, 2],
+                end_points[:, 0], end_points[:, 1], end_points[:, 2])
     ax.quiver(start_points[:, 0], start_points[:, 1], start_points[:, 2],
                 end_points[:, 0], end_points[:, 1], end_points[:, 2],
                 length = arrows_lenght, normalize = True)
@@ -50,7 +74,7 @@ def visualize_data_with_orientation(experiment_id, start_points, end_points, arr
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    plt.savefig('plots/experiment_{}.png'.format(experiment_id))
+    # plt.savefig('plots/experiment_{}.png'.format(experiment_id))
     plt.show()
 
 
@@ -64,6 +88,6 @@ def visualize_data(x_list, y_list, z_list):
     plt.show()
 
 
-experiment_id = 1
-starts, ends = read_experiment(experiment_id=experiment_id, num_samples=100)
-visualize_data_with_orientation(experiment_id, starts, ends, arrows_lenght=0.001)
+experiment_id = E_ID
+starts, ends = read_experiment(data_dir = 'data/data_may_26.csv', experiment_id=experiment_id, num_samples=1000000)
+# visualize_data_with_orientation(experiment_id, starts, ends, arrows_lenght=0.001)
