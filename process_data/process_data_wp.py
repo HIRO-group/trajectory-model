Fix rotation and bug for classifier_predict

process_data/process_data_wp.py
@@ -6,8 +6,8 @@ from trajectory_model.helper import find_significant_curvature_changes, \
 
 from trajectory_model.spill_free.constants import MAX_TRAJ_STEPS, EMBED_DIM
 from trajectory_model.informed_sampler.constants import MAX_NUM_WAYPOINTS
-from process_data import read_from_mocap_file, transform_trajectory, add_equivalent_quaternions, read_from_ompl_file
-from process_data import process_data as process_data_spill
+from trajectory_model.process_data import read_from_mocap_file, transform_trajectory, add_equivalent_quaternions, read_from_ompl_file
+from trajectory_model.process_data import process_data as process_data_spill
 from trajectory_model.helper import plot_X, plot_multiple_e_ids, plot_multiple_X
 
 
@@ -43,8 +43,9 @@ def select_waypoints(X):
         all_zero_indices = np.where(np.all(X_new[e_id, :, 3:7] == 0, axis=1))
         if len(all_zero_indices[0]) == 0:
             continue
-        last_non_zero_index = all_zero_indices[0][0] - 1
-        X_new[e_id, last_non_zero_index + 1:, :] = X_new[e_id, last_non_zero_index, :]
+        else:
+            last_non_zero_index = all_zero_indices[0][0] - 1
+            X_new[e_id, last_non_zero_index + 1:, :] = X_new[e_id, last_non_zero_index, :]
     return X_new
 
 
@@ -63,7 +64,6 @@ def prepare_model_input(X):
             if x_index > X.shape[1] - 1:
                 x_index = X.shape[1] - 1
             Y_new[e_id * data_per_experiment + i, :] = X[e_id, x_index, 0:EMBED_DIM-1]
-    
     return X_new, Y_new
 
 

trajectory_model/data.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from process_data import process_data
-from process_data_wp import process_data as process_data_wp
+from trajectory_model.process_data import process_data
+# from process_data_wp import process_data as process_data_wp
 
 
 def get_manual_train_and_val_index():
@@ -87,7 +87,32 @@ def get_orientation_wp_data(manual=False):
     return X_train, Y_train, X_val, Y_val, X, Y
 
 
+def normalize_data(X, Y):
+    mean_X = np.mean(X, axis=(0, 1), keepdims=True)
+    std_X = np.std(X, axis=(0, 1), keepdims=True)
+    mean_Y = np.mean(Y, axis=0, keepdims=True)
+    std_Y = np.std(Y, axis=0, keepdims=True)
+
+    X = (X - mean_X) / std_X
+    Y = (Y - mean_Y) / std_Y
+
+    print("mean_X: ", mean_X)
+    print("std_X: ", std_X)
+    print("mean_Y: ", mean_Y)
+    print("std_Y: ", std_Y)
+
+#     mean_X:  [[[-1.32117807e-17  3.80431987e-18 -2.07520454e-18 -9.18323003e-18
+#     2.67658802e-16 -5.69337829e-18 -1.41335447e-17  2.29643836e+00
+#     4.58630137e+00  6.00273973e-01]]]
+# std_X:  [[[0.48387692 0.08604237 0.18141209 0.12780175 0.39179178 0.18965058
+#    0.89127229 0.59099491 0.49249576 0.20547288]]]
+# mean_Y:  [[0.48219178]]
+# std_Y:  [[0.49968277]]
+
+    return X, Y
+
 def get_data():
     X, Y = process_data()
+    # X, Y = normalize_data(X, Y)
     X_train, Y_train, X_val, Y_val = get_train_and_val_set(X, Y)
     return X_train, Y_train, X_val, Y_val, X, Y

trajectory_model/helper.py
@@ -202,27 +202,27 @@ def plot_xyz(X_xyz, Y_xyz, e_id):
 
 
 def ctime_str():
-    dt = datetime.now()
-    now = dt.strftime("%H:%M:%S")
-    return now
+    # dt = datetime.now()
+    # now = dt.strftime("%H:%M:%S")
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-# import tensorflow as tf
-# class SaveBestAccuracy(tf.keras.callbacks.Callback):
-#     def __init__(self, file_address="best"):
-#         super().__init__()
-#         self.file_address = file_address
+import tensorflow as tf
+class SaveBestAccuracy(tf.keras.callbacks.Callback):
+    def __init__(self, file_address="best"):
+        super().__init__()
+        self.file_address = file_address
 
-#     def on_train_begin(self, logs=None):
-#         self.val_acc = []
+    def on_train_begin(self, logs=None):
+        self.val_acc = []
 
-#     def on_epoch_end(self, epoch, logs=None):
-#         min_epoch = 0
-#         current_train_acc = logs.get("accuracy")
-#         current_val_acc = logs.get("val_accuracy")
-#         self.val_acc.append(logs.get("val_accuracy"))
-#         if current_val_acc >= max(self.val_acc) and current_val_acc >= 0.87 and current_train_acc >= 0.87:
-#             min_epoch = epoch
-#             print(f'Found best accuracy. Saving entire model. Epoch: {min_epoch}')
-#             print('Val accuracy: ', current_val_acc, ', Train accuracy: ', current_train_acc)
-#             self.model.save_weights(f'weights/{self.file_address}/{ctime_str()}_epoch_{min_epoch}_best_val_acc_{round(current_val_acc, 2)}_train_acc_{round(current_train_acc, 2)}.h5')
+    def on_epoch_end(self, epoch, logs=None):
+        min_epoch = 0
+        current_train_acc = logs.get("accuracy")
+        current_val_acc = logs.get("val_accuracy")
+        self.val_acc.append(logs.get("val_accuracy"))
+        if current_val_acc >= max(self.val_acc) and current_val_acc >= 0.87 and current_train_acc >= 0.87:
+            min_epoch = epoch
+            print(f'Found best accuracy. Saving entire model. Epoch: {min_epoch}')
+            print('Val accuracy: ', current_val_acc, ', Train accuracy: ', current_train_acc)
+            self.model.save_weights(f'weights/{self.file_address}/best/{ctime_str()}_epoch_{min_epoch}_best_val_acc_{round(current_val_acc, 2)}_train_acc_{round(current_train_acc, 2)}.h5')

trajectory_model/process_data.py
@@ -1,7 +1,10 @@
 import os
 import csv
 import numpy as np
-from trajectory_model.spill_free.constants import MAX_TRAJ_STEPS, EMBED_DIM, DT
+from trajectory_model.spill_free.constants import MAX_TRAJ_STEPS, EMBED_DIM, DT, \
+    BIG_RADIUS, BIG_HEIGHT, SMALL_RADIUS, SMALL_HEIGHT, \
+    BIG_FILL_FULL, BIG_FILL_HALF, SMALL_FILL_FULL, SMALL_FILL_HALF
+
 from trajectory_model.helper import plot_X
 
 
@@ -9,27 +12,26 @@ DIR_PREFIX = '/home/ava/projects/trajectory-model/data/mocap_new/'
 
 # nospill, spill, radius, height, fill_level
 
-BIG_RADIUS = 3
-BIG_HEIGHT = 4
-SMALL_RADIUS = 1.8
-SMALL_HEIGHT = 5
 FILE_NAMES_NOSPILL_SPILL = \
-    [("big/full/spill-free/", "big/full/spilled/", BIG_RADIUS, BIG_HEIGHT, 0.8),
-        ("big/half-full/spill-free/", "big/half-full/spilled/", BIG_RADIUS, BIG_HEIGHT, 0.3),
-        ("small/full/spill-free/", "small/full/spilled/", SMALL_RADIUS, SMALL_HEIGHT, 0.8),
-        ("small/half-full/spill-free/", "small/half-full/spilled/", SMALL_RADIUS, SMALL_HEIGHT, 0.5)]
+    [("big/full/spill-free/", "big/full/spilled/", BIG_RADIUS, BIG_HEIGHT, BIG_FILL_FULL),
+        ("big/half-full/spill-free/", "big/half-full/spilled/", BIG_RADIUS, BIG_HEIGHT, BIG_FILL_HALF),
+        ("small/full/spill-free/", "small/full/spilled/", SMALL_RADIUS, SMALL_HEIGHT, SMALL_FILL_FULL),
+        ("small/half-full/spill-free/", "small/half-full/spilled/", SMALL_RADIUS, SMALL_HEIGHT, SMALL_FILL_HALF)]
 
 
 def read_a_file(file_path, radius, height, fill_level):
     X = np.zeros((1, 10000, EMBED_DIM), dtype=np.float64)
     trajectory_index = 0
-    with open(file_path, mode ='r') as file:
+    with open(file_path, mode='r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             keys = list(row.values())
-            x, y, z = np.float64(keys[1]), np.float64(keys[2]), np.float64(keys[3])
-            a, b, c, d = np.float64(keys[4]), np.float64(keys[5]), np.float64(keys[6]), np.float64(keys[7])
-            embedding = np.array([[x, y, z, a, b, c, d, radius, height, fill_level]])
+            y, x, z = np.float64(keys[1]), np.float64(
+                keys[2]), np.float64(keys[3])
+            a, b, c, d = np.float64(keys[4]), np.float64(
+                keys[5]), np.float64(keys[6]), np.float64(keys[7])
+            embedding = np.array(
+                [[x, y, z, a, b, c, d, radius, height, fill_level]])
             X[0, trajectory_index, :] = embedding
             # print("embedding: ", embedding)
             trajectory_index += 1
@@ -49,7 +51,8 @@ def read_a_directory(directory_path, radius, height, fill_level):
     files = os.listdir(directory_path)
     for file in files:
         file_path = directory_path + file
-        X_new = read_a_file(file_path, radius, height, fill_level) # single traj
+        X_new = read_a_file(file_path, radius, height,
+                            fill_level)  # single traj
         # print("X-new: ",  X_new[0:3, :, :])
         # input()
         X = np.concatenate((X, X_new), axis=0)
@@ -60,14 +63,16 @@ def read_a_directory(directory_path, radius, height, fill_level):
 
 def handle_nospill(nospill_file, radius, height, fill_level):
     file_path_no_spill = DIR_PREFIX + nospill_file
-    X = read_a_directory(file_path_no_spill, radius, height, fill_level) # multiple trajs
+    X = read_a_directory(file_path_no_spill, radius,
+                         height, fill_level)  # multiple trajs
     Y = np.zeros((X.shape[0], 1))
     return X, Y
 
 
 def handle_spill(spill_file, radius, height, fill_level):
     file_path_spill = DIR_PREFIX + spill_file
-    X = read_a_directory(file_path_spill, radius, height, fill_level) # multiple trajs
+    X = read_a_directory(file_path_spill, radius, height,
+                         fill_level)  # multiple trajs
     Y = np.ones((X.shape[0], 1))
     return X, Y
 
@@ -86,17 +91,18 @@ def read_from_files():
 
         X_s, Y_s = handle_spill(spill_file, radius, height, fill_level)
         X = np.concatenate((X, X_s), axis=0)
-        Y = np.concatenate((Y, Y_s), axis=0)    
+        Y = np.concatenate((Y, Y_s), axis=0)
     return X, Y
+
 
 def copy_last_non_zero_value(X):
     for e_id in range(X.shape[0]):
-        embedding = X[e_id, :, 0:7] 
+        embedding = X[e_id, :, 0:7]
         all_zero_indices = np.where(np.all(embedding == 0, axis=1))
         if len(all_zero_indices[0]) == 0:
             continue
-        first_zero_index =  all_zero_indices[0][0]
-        
+        first_zero_index = all_zero_indices[0][0]
+
         if first_zero_index == 0:
             X[e_id, :, :] = X[e_id-1, :, :]
         else:
@@ -126,9 +132,7 @@ def add_equivalent_quaternions(X, Y):
     return X_new, Y_new
 
 
-
 def add_partial_trajectory(X, Y):
-    num_experiments = X.shape[0]
     data_per_experiment = 4
     new_X_num_data = data_per_experiment * X.shape[0]
     X_new = np.zeros((new_X_num_data, X.shape[1], EMBED_DIM), dtype=np.float64)
@@ -137,13 +141,15 @@ def add_partial_trajectory(X, Y):
     for e_id in range(X.shape[0]):
         for i in range(data_per_experiment):
             Y_new[e_id * data_per_experiment + i, :] = Y[e_id]
-            X_new[e_id * data_per_experiment + i, 0: int((i+1) * X.shape[1]/data_per_experiment), :] = X[e_id, 0: int((i+1) * X.shape[1]/data_per_experiment), :]
+            X_new[e_id * data_per_experiment + i, 0: int(
+                (i+1) * X.shape[1]/data_per_experiment), :] = X[e_id, 0: int((i+1) * X.shape[1]/data_per_experiment), :]
             # fill the rest with the last value
-            X_new[e_id * data_per_experiment + i, int((i+1) * X.shape[1]/data_per_experiment):, :] = X[e_id, int((i+1) * X.shape[1]/data_per_experiment)-1, :]
+            X_new[e_id * data_per_experiment + i, int((i+1) * X.shape[1]/data_per_experiment):, :] = X[e_id, int(
+                (i+1) * X.shape[1]/data_per_experiment)-1, :]
     return X_new, Y_new
 
 
-def  add_delta_X(X):
+def add_delta_X(X):
     X_new = np.zeros((X.shape[0], MAX_TRAJ_STEPS, EMBED_DIM), dtype=np.float64)
     for e_id in range(X.shape[0]):
         for i in range(1, X.shape[1]):
@@ -152,7 +158,8 @@ def  add_delta_X(X):
 
 
 def add_reverse_X(X, Y):
-    X_new = np.zeros((2 * X.shape[0], MAX_TRAJ_STEPS, EMBED_DIM), dtype=np.float64)
+    X_new = np.zeros(
+        (2 * X.shape[0], MAX_TRAJ_STEPS, EMBED_DIM), dtype=np.float64)
     Y_new = np.zeros((2 * X.shape[0], 1), dtype=np.float64)
     for e_id in range(X.shape[0]):
         X_new[2 * e_id, :, :] = X[e_id, :, :]
@@ -160,7 +167,7 @@ def add_reverse_X(X, Y):
 
         X_new[2 * e_id + 1, :, 0:3] = - X[e_id, :, 0:3]
         X_new[2 * e_id + 1, :, 3:] = X[e_id, :, 3:]
-        
+
         Y_new[2 * e_id + 1] = Y[e_id]
     return X_new, Y_new
 
@@ -187,11 +194,11 @@ def process_data():
 
 if __name__ == "__main__":
     X, Y = process_data()
-#     print("X.shape:", X.shape)
-#     print("Y.shape:", Y.shape)
-#     print("here: ", X[0, :, :])
+        # print("X.shape:", X.shape)
+        # print("Y.shape:", Y.shape)
+        # print("here: ", X[0, :, :])
 
-#     plot_X(X, 0, 0.1)
+    plot_X(X, 3, 0.01)
 
 #     # w/o reverse:  [[ 0.    0.    0.   ...  3.    4.    0.8 ]
 # #  [-0.   -0.    0.   ...  3.    4.    0.8 ]
@@ -200,5 +207,3 @@ if __name__ == "__main__":
 # #  [-0.69  0.05 -0.15 ...  3.    4.    0.8 ]
 # #  [-0.69  0.05 -0.15 ...  3.    4.    0.8 ]
 # #  [-0.69  0.05 -0.15 ...  3.    4.    0.8 ]]
-    
-

trajectory_model/sampler_predict.py
@@ -1,130 +1,69 @@
 import numpy as np
-# from trac_ik_python.trac_ik import IK
+from trajectory_model.process_data import read_from_files, copy_last_non_zero_value, \
+    transform_trajectory, add_equivalent_quaternions, round_down_orientation_and_pos
+from trajectory_model.spill_free.constants import EMBED_DIM
+from trajectory_model.helper import plot_X, plot_multiple_e_ids, plot_multiple_X
+
+MAX_NUM_WAYPOINTS = 10
+
+def keep_spill_free(X, Y):
+    X_new, Y_new = [], []
+    for e_id in range(X.shape[0]):
+        if Y[e_id] == 0:
+            X_new.append(X[e_id])
+            Y_new.append(Y[e_id])
+    X_new = np.array(X_new)
+    Y_new = np.array(Y_new)
+    return X_new, Y_new
+
+def select_waypoints(X):
+    X_new = np.zeros((X.shape[0], MAX_NUM_WAYPOINTS, EMBED_DIM), dtype=np.float64)
+    for e_id in range(X.shape[0]):
+        embedding = X[e_id, :, 0:7]
+        all_zero_indices = np.where(np.all(embedding == 0, axis=1))
+
+        if  len(all_zero_indices[0]) != 0  and all_zero_indices[0][0] == 0:
+            X_new[e_id] = X_new[e_id - 1]
+            continue
+
+        if len(all_zero_indices[0]) == 0:
+            max_index = X.shape[1] - 1
+        else:
+            max_index = all_zero_indices[0][0] - 1 # The last non zero index
+
+        step_size = int(max_index / MAX_NUM_WAYPOINTS)
+        remainder = max_index % MAX_NUM_WAYPOINTS
+        X_new[e_id] = np.array([np.array(X[e_id, i, :]) for i in range(0, max_index - remainder, step_size)])
+
+        all_zero_indices = np.where(np.all(X_new[e_id, :, 3:7] == 0, axis=1))
+        if len(all_zero_indices[0]) == 0:
+            continue
+        else:
+            last_non_zero_index = all_zero_indices[0][0] - 1
+            X_new[e_id, last_non_zero_index + 1:, :] = X_new[e_id, last_non_zero_index, :]
+    return X_new
+    
+
+def transform_for_IK(X):
+    pass
 
 
-from trajectory_model.informed_sampler.model import PositionSampler, OrientationSampler
-from trajectory_model.informed_sampler.constants import EMBED_DIM_ORIENTATION_X, \
-    EMBED_DIM_ORIENTATION_Y, MAX_NUM_WAYPOINTS, \
-    EMBED_DIM_POS_X, EMBED_DIM_POS_Y, MAX_NUM_WAYPOINTS
-
-
-position_model = PositionSampler(max_num_waypoints=MAX_NUM_WAYPOINTS,
-                                 embed_X=EMBED_DIM_POS_X,
-                                 embed_Y=EMBED_DIM_POS_Y)
-position_model.compile(
-    optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-position_model.build((None, MAX_NUM_WAYPOINTS, EMBED_DIM_POS_X))
-position_model.load_weights(
-    "/home/ava/projects/trajectory-model/weights/position_sampler/val_0.89_train_0.92.h5")
-
-
-orientation_model = OrientationSampler(max_num_waypoints=MAX_NUM_WAYPOINTS,
-                                       embed_X=EMBED_DIM_ORIENTATION_X,
-                                       embed_Y=EMBED_DIM_ORIENTATION_Y)
-orientation_model.compile(
-    optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-orientation_model.build((None, MAX_NUM_WAYPOINTS, EMBED_DIM_ORIENTATION_X))
-orientation_model.load_weights(
-    "/home/ava/projects/trajectory-model/weights/orientation_sampler/09:18:26_epoch_596_best_val_acc_0.89_train_acc_0.79.h5")
-
-
-def get_position_data(trajectory):
-    trajectory_pos = []
-    for traj in trajectory:
-        trajectory_pos.append(traj[0:3])
-    return trajectory_pos
-
-
-def translate(trajectory):
-    xyz = trajectory[0, :, 0:3]  # shape: (T, 3)
-    xyz = xyz - trajectory[0, 0, 0:3]  # shape: (T, 3)
-    trajectory[0, :, 0:3] = xyz
-    return trajectory
-
-
-def convert_to_position_model_input(trajectory):
-    step_size = int(len(trajectory) / MAX_NUM_WAYPOINTS)
-    if step_size == 0:
-        step_size = 1
-
-    remainder = len(trajectory) % MAX_NUM_WAYPOINTS
-
-    if len(trajectory) < MAX_NUM_WAYPOINTS:
-        remainder = 0
-    trajectory = np.array([np.array(trajectory[i])
-                          for i in range(0, len(trajectory) - remainder, step_size)])
-
-    if len(trajectory) < MAX_NUM_WAYPOINTS:
-        trajectory = np.append(trajectory, np.zeros(
-            (MAX_NUM_WAYPOINTS - len(trajectory), 3)), axis=0)
-
-    trajectory = np.append(trajectory, np.ones(
-        (trajectory.shape[0], 1)), axis=1)
-    trajectory = trajectory.reshape(1, MAX_NUM_WAYPOINTS, EMBED_DIM_POS_X)
-    return trajectory
-
-
-def convert_to_orientation_model_input(trajectory, position_prediction):
-    step_size = int(len(trajectory) / MAX_NUM_WAYPOINTS)
-    if step_size == 0:
-        step_size = 1
-
-    remainder = len(trajectory) % MAX_NUM_WAYPOINTS
-
-    if len(trajectory) < MAX_NUM_WAYPOINTS:
-        remainder = 0
-    trajectory = np.array([np.array(trajectory[i])
-                          for i in range(0, len(trajectory) - remainder, step_size)])
-    trajectory = np.vstack((trajectory, np.array(
-        [position_prediction[0], position_prediction[1], position_prediction[2], 0, 0, 0, 0])))
-
-    if len(trajectory) < MAX_NUM_WAYPOINTS:
-        trajectory = np.append(trajectory, np.zeros(
-            (MAX_NUM_WAYPOINTS - len(trajectory), 7)), axis=0)
-
-    trajectory = np.append(trajectory, np.ones(
-        (trajectory.shape[0], 1)), axis=1)
-    trajectory = trajectory.reshape(
-        1, MAX_NUM_WAYPOINTS, EMBED_DIM_ORIENTATION_X)
-    return trajectory
+X, Y = read_from_files()
+X, Y = keep_spill_free(X, Y)
+X = copy_last_non_zero_value(X)
+X = transform_trajectory(X)
+X = transform_for_IK(X)
+X, Y = add_equivalent_quaternions(X, Y)
+X = round_down_orientation_and_pos(X)
+X = select_waypoints(X)
 
 
 def sample_state(trajectory):
-    trajectory_pos = get_position_data(trajectory)
-    trajectory_pos = convert_to_position_model_input(trajectory_pos)
-    trajectory_pos = translate(trajectory_pos)
-    position_prediction = position_model.predict(trajectory_pos)[0]
+    # Should sample from the specific cup properties
+    random_e_id = np.random.randint(0, X.shape[0])
+    random_traj_step = np.random.randint(0, X.shape[1])
+    return X[random_e_id, random_traj_step, 0:7]
 
-    trajectory = convert_to_orientation_model_input(
-        trajectory, position_prediction)
-    trajectory = translate(trajectory)
-    orientation_prediction = orientation_model.predict(trajectory)[0]
 
-    state = [position_prediction[0], position_prediction[1], position_prediction[2], orientation_prediction[0],
-             orientation_prediction[1], orientation_prediction[2], orientation_prediction[3]]
-    # print("state cartesian: ", state)
-
-    # print("before ik solver")
-
-    # # try: 
-    # ik_solver = IK("panda_link0", "panda_link8")
-    # # except Exception as e:
-    # #     print("Exception in ik_solver: ", e)
-
-    # print("after ik solver")
-    # lower_bound, upper_bound = ik_solver.get_joint_limits()
-    # print("lower_bound: ", lower_bound)
-    # print("upper_bound: ", upper_bound)
-    # print("ik_solver number of joints: ", ik_solver.number_of_joints)
-
-    # seed_state = [0.0] * ik_solver.number_of_joints
-    # joint_angles = ik_solver.get_ik(seed_state,
-    #                                 position_prediction[0], position_prediction[1],
-    #                                 position_prediction[3], orientation_prediction[0],
-    #                                 orientation_prediction[1], orientation_prediction[2],
-    #                                 orientation_prediction[3])
-
-    # print("joint_angles: ", joint_angles)
-    
-    # return [0.5295887589454651, -0.16105137765407562, 0.296753466129303, 0.004732200410217047, -0.018793363124132156, 0.03531074523925781, -0.9991884827613831]
-    return state
+xyzabcd = sample_state(0)
+print("xyzabcd: ", xyzabcd)